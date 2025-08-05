b9069640e6f7d25e373343296b04409771be99d6
MUHAMAD SYAHMIE BIN MOHD RAZI <syahmierazi97@gmail.com>
2025-08-05 10:54:55 +0000

    test

--- backend-vista/src/routes/companyRoutes.ts
+++ backend-vista/src/routes/companyRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { createCompany, getCompanies } from "../controllers/companyController";
 
 const router = Router();
+
 router.post("/", createCompany);
 router.get("/", getCompanies);
 
